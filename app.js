311ff0945bb4ba87ebb03ea7bfbca7491ff80227
rogelio1502 <rogelio15021515@gmail.com>
2022-01-11 19:03:42 -0600

    ready to deploy

---
+++ app.js
@@ -0,0 +1,28 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+//app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+	res.status(200).end(
+		JSON.stringify(
+			{
+				"name":"Rogelio",
+				"age":21,
+				"intereses":"Technology"
+				
+			},
+			null,
+			4
+
+		)
+	);
+	
+   })
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
